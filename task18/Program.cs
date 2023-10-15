02e67875e0b61b43fd4da540cd9a53ae4cb378e6
Mikhail <osharovmisha@rambler.ru>
2023-10-15 21:28:00 +0700

    task18

---
+++ task18/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Задача 18: Напишите программу, которая по заданному номеру четверти, показывает
+диапазон возможных координат точек в этой четверти (x и y)
+*/
+
+string f(int a)
+{
+    if(a == 1)
+       return"x > 0 and y > 0";
+    else if(a == 2)
+       return"x < 0 and y > 0";
+    else if(a == 3)
+       return"x < 0 and y < 0";
+    return "x > 0 and y < 0";
+}
+System.Console.Write("Введите номер четверти: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine(f(x));
