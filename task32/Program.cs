5dd4eef5acacc3e3080d2b8d3ea61571c4d29249
Mikhail <osharovmisha@rambler.ru>
2023-10-20 18:20:08 +0700

    task32

---
+++ task32/Program.cs
@@ -0,0 +1,56 @@
+﻿/*
+Напишите программу замена элементов массива: положительные элементы замените на
+соответствующие отрицательные, и наоборот.
+[-4, -8, 8, 2] -> [4, 8, -8, -2]
+*/
+
+using System;
+Console.Clear();
+
+int[] GetRandomArray(int start, int end, int lenght)
+{
+    int[] array = new int[lenght];
+    for (int i = 0; i < lenght; i++)
+    {
+        array[i] = new Random().Next(start, end + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    System.Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i]);
+        if (i < array.Length - 1)
+            System.Console.Write(", ");
+    }
+    System.Console.WriteLine("]");
+}
+
+int[] ChangePosAndNegArray(int[] array)
+{
+    int[] result = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        result[i] = array[i] * -1;
+    }
+    return result;
+}
+
+int TakeEntNum(string message)
+{
+    System.Console.WriteLine(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+int userArraySize = TakeEntNum("Введите размер массива:");
+int userArrayStart = TakeEntNum("Введите диапазон начальных номеров массива:");
+int userArrayEnd = TakeEntNum("Введите диапазон конечных номеров массива:");
+
+int[] userArray = GetRandomArray(userArrayStart, userArrayEnd, userArraySize);
+int[] userChangedArray = ChangePosAndNegArray(userArray);
+PrintArray(userArray);
+PrintArray(userChangedArray);
