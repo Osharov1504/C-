9dff4f6dd25c9d150128dddc26022e3c0a9946c3
Mikhail <osharovmisha@rambler.ru>
2023-10-20 18:18:45 +0700

    task229

---
+++ task29/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
+
+namespace RandomArray
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr = new int[8];
+
+
+            Random random = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = random.Next();
+            }
+
+            Console.Write("Массив: ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"{arr[i]} ");
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
