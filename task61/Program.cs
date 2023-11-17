8193652ef2aca96a24244d685e569f6950c60b56
Mikhail <osharovmisha@rambler.ru>
2023-11-17 09:25:31 +0700

    task61

---
+++ task61/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Вывести первые N строк треугольника Паскаля. Сделать вывод в виде равнобедренного треугольника
+*/
+
+class Program
+{
+static void Main(string[] args)
+{
+Console.Clear();
+System.Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+int[] triangle = new int[N];
+triangle[0] = 1;
+for (int i = 1; i < N; i++)
+{
+triangle[i] = triangle[i-1] + 1;
+}
+for (int j = 0; j < N; j++)
+{
+for (int k = 0; k <= j; k++)
+{
+Console.Write($"{triangle[k]} ");
+}
+Console.WriteLine();
+}
+}
+}
