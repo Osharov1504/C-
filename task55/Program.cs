d9d5bee26b179bb55ffe2caf4cc165498e34c53b
Mikhail <osharovmisha@rambler.ru>
2023-11-17 09:23:57 +0700

    task55

---
+++ task55/Program.cs
@@ -0,0 +1,83 @@
+﻿/*
+Задайте двумерный массив. Напишите программу,
+которая заменяет строки на столбцы. В случае, если это
+невозможно, программа должна вывести сообщение для
+пользователя.
+*/
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите количество строк массива: ");
+        int rows = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Введите количество столбцов массива: ");
+        int cols = Convert.ToInt32(Console.ReadLine());
+
+        int[,] matrix = new int[rows, cols];
+
+        Random random = new Random();
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                matrix[i, j] = random.Next(1, 100);
+            }
+        }
+
+        Console.WriteLine("\nИсходная массива:");
+             
+        PrintMatrix(matrix);
+
+        if (rows == cols)
+        {
+            TransposeMatrix(matrix);
+            Console.WriteLine("\nМассив после замены строк на столбцы:");
+            PrintMatrix(matrix);
+        }
+        else
+        {
+            Console.WriteLine("\nНевозможно заменить строки на столбцы для данного массива.");
+        }
+    }
+
+    static void PrintMatrix(int[,] matrix)
+    {
+         int rows = matrix.GetLength(0);
+         int cols = matrix.GetLength(1);
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                Console.Write(matrix[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+       
+static void TransposeMatrix(int[,] matrix)
+    {
+        int rows = matrix.GetLength(0);
+        int cols = matrix.GetLength(1);
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = i + 1; j < cols; j++)
+            {
+                
+            {
+               
+                int temp = matrix[i, j];
+                matrix[i, j] = matrix[j, i];
+                matrix[j, i] = temp;
+            }
+        }
+    }
+}
+}
+          
