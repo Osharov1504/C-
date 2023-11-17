8e27c7f243361cfc24801713e1cd2abf97d13159
Mikhail <osharovmisha@rambler.ru>
2023-11-17 09:26:32 +0700

    task67

---
+++ task67/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+*/
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите число: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        int sum = SumOfDigits(number);
+
+        Console.WriteLine($"Сумма цифр числа {number} равна: {sum}");
+
+        Console.ReadLine();
+    }
+
+    static int SumOfDigits(int num)
+    {
+        if (num == 0)
+        {
+            return 0;
+        }
+        else
+        {
+            return num % 10 + SumOfDigits(num / 10);
+        }
+    }
+}
