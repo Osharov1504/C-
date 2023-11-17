6209d4b90ac279149276b8d9dfe2ee636ae978eb
Mikhail <osharovmisha@rambler.ru>
2023-11-17 09:26:01 +0700

    task63

---
+++ task63/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+*/
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите значение N: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+
+        Console.WriteLine($"Натуральные числа от 1 до {n}:");
+        for (int i = 1; i <= n; i++)
+        {
+            Console.Write(i + " ");
+        }
+
+        Console.ReadLine(); 
+    }
+}
